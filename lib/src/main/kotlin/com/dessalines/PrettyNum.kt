376d4553964de0f7cdaeda1ad70ad4cacfe9d0aa
jonbation <jonbation4@gmail.com>
2025-06-09 20:48:16 -0400

    Lint 1

--- lib/src/main/kotlin/com/dessalines/PrettyNum.kt
+++ lib/src/main/kotlin/com/dessalines/PrettyNum.kt
@@ -12,21 +12,19 @@ fun prettyFormat(number: Long): String {
         number.toString()
     } else {
         val sign = if (number < 0) -1f else 1f
-        var number_as_float = abs(number).toFloat()
+        var numAsFloat = abs(number).toFloat()
 
         for (suffix in SUFFIXES) {
-            number_as_float /= 1000f
+            numAsFloat /= 1000f
 
-            if (number_as_float < 1000f) {
+            if (numAsFloat < 1000f) {
                 val decimalPlaces =
-                    if ((number_as_float - floor(number_as_float)) < 0.1 ||
-                        number_as_float >= 100f
-                    ) {
+                    if ((numAsFloat - floor(numAsFloat)) < 0.1 || numAsFloat >= 100f) {
                         0
                     } else {
                         1
                     }
-                val numStr = "%,.${decimalPlaces}f".format(sign * number_as_float)
+                val numStr = "%,.${decimalPlaces}f".format(sign * numAsFloat)
                 return "$numStr$suffix"
             }
         }
